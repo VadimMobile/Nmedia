9a8a8c3d8471fe0fd8d0d792379adab7a0c11bb3
VadimMobile <qwerty901cx@gmail.com>
2025-05-12 00:00:44 +0300

    Image loading added

--- app/src/main/java/ru/netology/nmedia/activity/ImageFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/ImageFragment.kt
@@ -1,4 +1,33 @@
 package ru.netology.nmedia.activity
 
-class ImageFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.netology.nmedia.BuildConfig.BASE_URL
+import ru.netology.nmedia.databinding.FragmentImageBinding
+import ru.netology.nmedia.view.load
+
+class ImageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        val binding = FragmentImageBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+
+        binding.apply {
+            attachmentAll.visibility = View.GONE
+            attachmentAll.load("${BASE_URL}/media/${arguments?.getString("image")}")
+            attachmentAll.visibility = View.VISIBLE
+        }
+
+        return binding.root
+    }
 }
